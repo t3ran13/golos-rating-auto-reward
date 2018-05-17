92c5c15f81404d915c5b097ca628ac376e9897a5
t3ran13 <teran-555@yandex.ru>
2018-05-17 12:12:18 +0300

    added latency for block explorer

--- Processes/BlockchainExplorerProcess.php
+++ Processes/BlockchainExplorerProcess.php
@@ -26,4 +26,11 @@ class BlockchainExplorerProcess extends \GolosPhpEventListener\app\process\Block
     {
         $this->connector = new $this->connectorClassName(1);
     }
+
+    public function getCurrentBlockNumber()
+    {
+        $currentBlockNumber = parent::getCurrentBlockNumber();
+
+        return $currentBlockNumber - 10;
+    }
 }
